2467988e2c7803d2f126348ccbc26273edc12fa5
Aslin Gijo <90519507+AslinGijo-GM@users.noreply.github.com>
2025-03-23 19:55:28 +0530

    Create fulladder.sv

---
+++ FULL ADDER/fulladder.sv
@@ -0,0 +1,20 @@
+// module fulladder(
+// 	input a,b,c,
+//     output sum,carry
+// );
+  
+//   assign sum = a^b^c;
+//   assign carry = (a&b)|(b&c)|(a&c);
+  
+// endmodule
+
+
+
+module fulladder(
+	intf intff
+);
+  
+  assign intff.sum = intff.a^intff.b^intff.c;
+  assign intff.carry = (intff.a&intff.b)|(intff.b&intff.c)|(intff.a&intff.c);
+  
+endmodule
